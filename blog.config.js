7681b2d1f5ff6cbd6ee2c81d5f8814d90d90d2bb
coko8886 <lucygood890@gmail.com>
2025-04-08 11:22:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '勿忘我博客', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个闲杂人员', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://good8.zone.id/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '勿忘我博客, 勿忘我blog', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || 'https://tc.668884.xyz/file/1744080150694_10 (2).webp', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
